d60b35719a2599e409d992af1062f0c11d716e88
gyujing <1794755893@qq.com>
2023-08-14 22:13:13 +0800

    学习

--- express/router.js
+++ express/router.js
@@ -1,46 +1,28 @@
-var express = require('express'); 
-var history = require('connect-history-api-fallback');
-var app = express(); 
-var PORT = 3001; 
-  
-// Single routing 
-var router = express.Router(); 
-    
-router.get('/', function (req, res, next) { 
-    console.log("Router Working"); 
-    // req.url = './index.html';
-    // next();
-    res.end(); 
-}) 
+var express = require("express");
+var history = require("connect-history-api-fallback");
+var path = require("path");
 
-var router1 = express.Router(); 
-var router2 = express.Router(); 
-var router3 = express.Router(); 
-  
+var app = express();
 
-router1.get('/user', function (req, res, next) { 
-  console.log("User Router Working"); 
-  res.end(); 
-}); 
- 
-router2.get('/admin', function (req, res, next) { 
-  console.log("Admin Router Working"); 
-  res.end(); 
-}); 
+var PORT = 3001;
 
-router3.get('/student', function (req, res, next) { 
-  console.log("Student Router Working"); 
-  res.end(); 
-}); 
+// Single routing
+var router = express.Router();
 
-// app.use(history); 
-app.use(router); 
+router.get("/", function (req, res, next) {
+  console.log("Router Working");
+  //   req.url = "./index.html";
+  //     next();
 
-app.use(router1); 
-app.use(router2); 
-app.use(router3); 
-  
-app.listen(PORT, function(err){ 
-    if (err) console.log(err); 
-    console.log("Server listening on PORT,http://127.0.0.1:"+PORT); 
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// app.use(history);
+app.use(router);
+
+app.use(express.static(path.join(__dirname, "./")));
+
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT,http://127.0.0.1:" + PORT);
 });
